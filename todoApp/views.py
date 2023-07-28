c5712e2705311164420746b62e25d1af5d7ba176
huseyintekin <huseyintekin10@gmail.com>
2023-07-28 23:51:28 +0200

    as

--- todoApp/views.py
+++ todoApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import *
+from django.shortcuts import get_object_or_404
 
 from.models import Todo
 from .serializers import TodoSerializer
@@ -28,4 +29,18 @@ def todo_list_create(request):
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-@
+@api_view(["GET", "PUT", "DELETE"])
+
+def todo_get_delete_update(request,pk):
+    if request.method=="GET":
+        todo=get_object_or_404(Todo,pk)                    
+        serializer=TodoSerializer(todo)
+        return Response(serializer.data)
+    
+    elif request.method=="PUT":                    
+        serializer=TodoSerializer(data=request.data, instance=todo)
+        if serializer.is_valid():
+            serializer.save()
+            message={"message":"Successfully Updated"}
+            return Response(message,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors,status=status.H)
